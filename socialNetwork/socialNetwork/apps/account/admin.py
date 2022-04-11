158331ea8a4c2a5f1deb0f797e3be510517fb8aa
Nikita <streamnik666nik@gmail.com>
2022-04-11 10:42:05 +0700

    qwe

--- socialNetwork/socialNetwork/apps/account/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Profile, Friend, Follower, Status
-
-
-admin.site.register(Profile)
-admin.site.register(Status)
-admin.site.register(Friend)
-admin.site.register(Follower)
--- socialNetwork/socialNetwork/apps/account/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from django.contrib.auth.models import User
-from .models import Profile
-
-class ProfileForm(ModelForm):
-    class Meta:
-        model = Profile
-        fields = ('avatar', 'gender', 'city', 'birth_date')
